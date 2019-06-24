5209f72982beb13d01e1daec5d08d5961ffae401
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-06-24 03:17:29 -0500

    Initial commit

---
+++ src/script.ts
@@ -0,0 +1,117 @@
+import { fromEntries, hasOwnProperty } from "./util";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
+
+const fileInput = document.getElementById("files") as HTMLInputElement;
+const versionInput = document.getElementById("version") as HTMLInputElement;
+const processButton = document.getElementById("process") as HTMLButtonElement;
+const progress = document.getElementById("progress") as HTMLProgressElement;
+const currentFile = document.getElementById("current") as HTMLParagraphElement;
+const saveButton = document.getElementById("save") as HTMLButtonElement;
+const resetButton = document.getElementById("reset") as HTMLButtonElement;
+
+const mainSection = document.getElementById("main") as HTMLDivElement;
+const processingSection = document.getElementById(
+  "processing"
+) as HTMLDivElement;
+const errorSection = document.getElementById("error") as HTMLDivElement;
+const doneSection = document.getElementById("done") as HTMLDivElement;
+
+function hide(...elems: HTMLElement[]) {
+  for (const elem of elems) elem.classList.add("hidden");
+}
+function show(...elems: HTMLElement[]) {
+  for (const elem of elems) elem.classList.remove("hidden");
+}
+
+function reset() {
+  show(mainSection);
+  hide(doneSection, errorSection, processingSection);
+}
+
+function err(error: string) {
+  errorSection.textContent = error;
+  show(errorSection);
+  hide(processingSection);
+  return new Error(error);
+}
+
+interface Index {
+  objects: {
+    [filename: string]: {
+      hash: string;
+      size: number;
+    };
+  };
+}
+
+processButton.addEventListener("click", async () => {
+  reset();
+
+  const files = fromEntries(
+    [...fileInput.files!].map(file => [
+      (file as any).webkitRelativePath as string,
+      file
+    ])
+  );
+  const version = versionInput.value;
+
+  const indexFilename = `assets/indexes/${version}.json`;
+  if (!hasOwnProperty(files, indexFilename)) {
+    console.log(files);
+    const versions = Object.keys(files)
+      .map(filename => {
+        const m = filename.match(/^assets\/indexes\/(.+)\.json$/);
+        return m && m[1];
+      })
+      .filter(a => !!a)
+      .join(", ");
+    return err(
+      `Couldn't find index for version provided. The available versions are: ${versions}`
+    );
+  }
+  const indexFile = files[indexFilename];
+  const index: Index = await new Response(indexFile).json();
+
+  const zip = new JSZip();
+
+  const musicFiles = Object.entries(index.objects).filter(
+    ([filename]) =>
+      filename.startsWith("minecraft/sounds/music/") ||
+      filename.startsWith("minecraft/sounds/records/")
+  );
+  for (const [filename, { hash }] of musicFiles) {
+    const m = filename.match(/\/([^\/]*?\.ogg$)/);
+    if (!m) {
+      throw err(`${filename} is not a .ogg file`);
+    }
+    const basename = m[1];
+
+    const musicFilename = `assets/objects/${hash.slice(0, 2)}/${hash}`;
+    if (!hasOwnProperty(files, musicFilename)) {
+      throw err(`File ${filename} with hash ${hash} not found`);
+    }
+    const file = files[musicFilename];
+
+    if (zip.file(basename)) {
+      throw err(`File ${basename} occurs twice`);
+    }
+    zip.file(basename, file);
+  }
+
+  show(processingSection);
+  zipFile = await zip.generateAsync({ type: "blob" }, meta => {
+    progress.value = meta.percent;
+    currentFile.textContent = `Currently processing: ${meta.currentFile}`;
+  });
+  hide(processingSection, mainSection);
+  show(doneSection);
+});
+
+let zipFile: Blob | null = null;
+
+saveButton.addEventListener("click", () => {
+  saveAs(zipFile!, "minecraft-music.zip");
+});
+
+resetButton.addEventListener("click", reset);
---
+++ src/util.ts
@@ -0,0 +1,30 @@
+export const hasOwnProperty = (
+  obj: Object,
+  v: string | number | symbol
+): boolean => Object.prototype.hasOwnProperty.call(obj, v);
+
+export function fromEntries<K extends PropertyKey, V>(
+  iter: Iterable<[K, V]>
+): { [k in K]: V } {
+  const obj = {};
+
+  for (const pair of iter) {
+    if (Object(pair) !== pair) {
+      throw new TypeError("iterable for fromEntries should yield objects");
+    }
+
+    // Consistency with Map: contract is that entry has "0" and "1" keys, not
+    // that it is an array or iterable.
+
+    const { "0": key, "1": val } = pair;
+
+    Object.defineProperty(obj, key, {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: val
+    });
+  }
+
+  return obj as any;
+}
